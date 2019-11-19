c67590a4dec2975ebef309e0b81802c52bbc9bc7
Kostas Kolomeets <kostas.kolomeets@intercom.io>
2019-11-19 11:16:02 +0200

    Update error message, add tests to filename parsing

--- app/cmd/post_test.go
+++ app/cmd/post_test.go
@@ -1,6 +1,11 @@
 package cmd
 
-import "testing"
+import (
+	"errors"
+	. "github.com/onsi/gomega"
+	"testing"
+	"time"
+)
 
 func Test_fNameWithoutExtension(t *testing.T) {
 	type args struct {
@@ -19,6 +24,42 @@ func Test_fNameWithoutExtension(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := fNameWithoutExtension(tt.args.fn); got != tt.want {
 				t.Errorf("fNameWithoutExtension() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestExtractMetaFromFilename(t *testing.T) {
+	tests := []struct {
+		filename string
+		want     Post
+		err      error
+	}{
+		{
+			"2019-11-17-Welcome.md",
+			Post{"", "", time.Date(2019, 11, 17, 0, 0, 0, 0, time.UTC), "Welcome"},
+			nil,
+		},
+		{
+			"FilenameWithoutDate.md",
+			Post{},
+			errors.New("can't parse filename 'FilenameWithoutDate.md', it should be in format 'YYYY-MM-DD-slug.md'"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.filename, func(t *testing.T) {
+			g := NewGomegaWithT(t)
+
+			post, err := ExtractMetaFromFilename(tt.filename)
+
+			if tt.err != nil {
+				g.Expect(err).To(Equal(tt.err))
+				g.Expect(post).To(Equal(Post{}))
+			} else {
+				g.Expect(err).To(BeNil())
+				g.Expect(post.Date).To(Equal(tt.want.Date))
+				g.Expect(post.Slug).To(Equal(tt.want.Slug))
 			}
 		})
 	}
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/freetonik/underblog
 go 1.13
 
 require (
+	github.com/onsi/gomega v1.7.1
 	github.com/radovskyb/watcher v1.0.7
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/smartystreets/goconvey v1.6.4 // indirect
 	gopkg.in/russross/blackfriday.v2 v2.0.0
 )
